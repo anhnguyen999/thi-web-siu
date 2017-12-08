f17f46ca879b2d25e9e0a1209c10d2421b90680f
nguyen luan <luannguyeniu2011@gmail.com>
2017-12-08 17:02:08 +0700

    Nguyen Van Luan

---
+++ NguyenVanLuan/NguyenVanLuan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NguyenVanLuan.Startup))]
+namespace NguyenVanLuan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
